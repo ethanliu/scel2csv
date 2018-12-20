5064bc9087a434ef196a14d00d8966cbdb7f22b9
Ethan Liu <ethan@creativecrap.com>
2018-12-20 18:14:03 +0800

    Initial Commit

---
+++ scel2csv/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  scel2csv
+//
+//  Created by Ethan Liu on 2018/12/20.
+//  Copyright © 2018 Ethan Liu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
